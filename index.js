529fa00a505c48b5a3b22106503242eb56e15387
Yu Zheng (Sam) <lesliesam@hotmail.com>
2024-06-04 11:52:18 +0000

    Project init.

---
+++ index.js
@@ -0,0 +1,18 @@
+import OpenAI from "openai";
+import { config } from 'dotenv';
+
+config();
+
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
+}
+
+main();
